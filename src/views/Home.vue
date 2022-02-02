25f25298d5ba2f11dd3b26f43f093a9c914c5110
DevSecOpsGit <roderik.lima@gmail.com>
2022-02-02 10:58:56 -0300

    views

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,7 +13,7 @@
     <div v-if="!user" class="updates">
       <div class="container">
         <h2>never miss a post. Register for your free account today!</h2>
-        <router-link class="router-button" to="#"> Register for CMXBlog  <Arrow class="arrow arrow-light" /> </router-link>
+        <router-link class="router-button" to="Login"> Register for CMXBlog  <Arrow class="arrow arrow-light" /> </router-link>
       </div>
     </div>
   </div>
