236c2b42e01f1311a521b20239349efb39dcdaef
DevSecOpsGit <roderik.lima@gmail.com>
2022-02-02 08:58:57 -0300

    >>>

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,8 @@
-// vue.config.js file to be placed in the root of your repository
 
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/' + process.env.cmxblog.io + '/'
-    : '/'
-}
 
 // vue.config.js file to be placed in the root of your repository
 
-module.exports = {
-  pwa: {
-      workboxOptions: {
-        exclude: [/_redirects/]
-      }
-    }
-}
+
 
 
 module.exports = {
