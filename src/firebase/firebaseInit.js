ce51cc5fe722ad0eb7f8dfafcfa6592a0c1ff0a9
DevSecOpsGit <roderik.lima@gmail.com>
2022-02-03 01:49:40 -0300

    master

--- src/firebase/firebaseInit.js
+++ src/firebase/firebaseInit.js
@@ -3,13 +3,7 @@ import "firebase/firestore";
 
 var firebaseConfig = {
   // ENTER YOUR CONFIG HERE
-  apiKey: "AIzaSyDDNcxWkW1Nk3nQiFvHFEVjDyQJJ2b7Vjc",
-  authDomain: "cmxblog.firebaseapp.com",
-  projectId: "cmxblog",
-  storageBucket: "cmxblog.appspot.com",
-  messagingSenderId: "640281981052",
-  appId: "1:640281981052:web:64a0672a9703a7c5e3d9f9",
-  measurementId: "G-NJXRFJVN3H"
+ 
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
